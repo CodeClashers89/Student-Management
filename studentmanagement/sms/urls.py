35633909443e80ba58abce68ad75dd9228fc1cd1
Neel0289 <neel9086@gmail.com>
2026-02-05 13:17:22 +0530

    good

--- studentmanagement/sms/urls.py
+++ studentmanagement/sms/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/login', views.login, name='login'),
+    path('login/', views.login, name='login'),
 ]
