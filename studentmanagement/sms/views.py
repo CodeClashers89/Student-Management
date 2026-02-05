cbcddeebbd4c3eb029203386e3d6cec0758ddd98
sayja-yug <yugsayja312@gmail.com>
2026-02-05 14:18:53 +0530

    created base.html

--- studentmanagement/sms/views.py
+++ studentmanagement/sms/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required
 def home(request):
     return render(request, 'student/student_dashboard.html')
 
 def login(request):
-    return render(request, 'student/login.html')
+    return render(request, 'login.html')
+
+def newtest(request):
+    return render(request, 'student/newtest.html')
